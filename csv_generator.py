4386f8d43a89f1fcfea670a54e4b80481f91bbd9
ali abbas <itsgeniusstar@gmail.com>
2017-02-20 00:00:35 +0500

    issue resolved

--- csv_generator.py
+++ csv_generator.py
@@ -3,6 +3,9 @@ __author__ = 'ali'
 from datetime import datetime
 import csv
 import os
+import sys
+import io
+
 def isNotEmpty(s):
     return bool(s and s.strip())
 
@@ -19,21 +22,34 @@ def generate_keys(fields):
 def generate_columns(fields):
     cols = []
     for k,v in fields:
-        cols.append(v)
+        #print(str(v))
+        try:
+            cols.append(str(v))
+        except:
+            print('')
+
     return cols
 
 def generate_csv(start_date,end_date,fields,data,module):
     create_dir_if_not_exist(module)
     #csv_file = module+'/'+module.capitalize()+'_'+start_date+'_'+end_date+'_'+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+'.csv'
     csv_file = module+'/'+module.capitalize()+'_'+start_date+'_'+end_date+'_'+datetime.now().strftime('%Y-%b-%d')+'.csv'
-
-
+  
     #csv_file = module+'/test.csv'
 
     #field_keys = sorted(fields.keys())
-    with open(csv_file, 'wb') as csvfile:
+    with open(csv_file, 'w') as csvfile:
         csvwriter = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        csvwriter.writerow(generate_columns(fields))
+
+        columns = generate_columns(fields)
+
+        if sys.version_info.major >= 3:
+            writer_file =  io.StringIO()
+        else:
+            writer_file =  io.BytesIO()
+
+        csvwriter.writerow(columns)
+
 
         for k in data['records']:
             row = []
